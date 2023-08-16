3df0c06a20a8ddfcfb8b440c7cab738a2729777c
marlon1925 <117753973+marlon1925@users.noreply.github.com>
2023-08-16 09:50:01 -0500

    funcion recuperar texto ok

--- ejercicio git/ejercicio.js
+++ ejercicio git/ejercicio.js
@@ -1,3 +1,6 @@
 saludar = function(){
     console.log("Ingrese el saludo");
+    let cmpImagenSaludo;
+    cmpImagenSaludo = document.getElementById("imgSaludo")
+    cmpImagenSaludo.src="./imagenes/Rotating_earth_(large).gif"
 }
---
+++ utilitarios/saludo.js
@@ -0,0 +1,13 @@
+saludar = function(){
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+
+}
+
+recuperarTexto=function(idComponente){
+    let componente;
+    let valorIngresado;
+    componente = document.getElementById(idComponente);
+    valorIngresado=componente.value;
+    return valorIngresado;
+}
