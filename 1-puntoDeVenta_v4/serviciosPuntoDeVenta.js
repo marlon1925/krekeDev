25c39c1bf707d6ddd941323b42d14df9da109466
marlon1925 <117753973+marlon1925@users.noreply.github.com>
2023-08-16 11:04:22 -0500

    funcion mostrar subtotal implementada

---
+++ 1-puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -0,0 +1,25 @@
+calcularValorDescuento = function (monto, porcentajeDescuento) {
+    let descuento;
+
+    descuento = (monto * porcentajeDescuento) / 100;
+
+    return descuento;
+}
+
+calcularIVA = function (monto) {
+    let valorIVA;
+    valorIVA = (monto * 12) / 100
+    return porcentajeIVA;
+}
+
+calcularSubTotal = function(precio, cantidad){
+    let subTotal;
+    subTotal = precio * cantidad
+    return subTotal;
+}
+
+calcularTotal = function(subTotal, descuento, iva){
+    let total;
+    total = (subTotal - descuento) + iva
+    return total;
+}
--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -7,36 +7,6 @@ saludar = function(){
     let mensajeBienvenida = "Bienvendo " + nombre + " " + apellido
     mostarTexto("lblResultado" , mensajeBienvenida)
     mostrarImagen("imgSaludar", "./image/saludo.gif")
+    mostarTextoEnCaja("txtNombre", "")
 }
 
-mostarTexto = function(idComponente, mensaje){
-    let componente
-    componente = document.getElementById(idComponente);
-    componente.innerText = mensaje;
-}
-
-mostrarImagen = function(idComponente, rutaImagen){
-    let componente
-    componente = document.getElementById(idComponente);
-    componente.src = rutaImagen
-}
-
-recuperarTexto=function(idComponente){
-    let componente;
-    let valorIngresado;
-    componente = document.getElementById(idComponente);
-    valorIngresado=componente.value;
-    return valorIngresado;
-}
-
-recuperarInt = function(idComponente){
-    let valorCaja =  recuperarTexto(idComponente);
-    let valorInt = parseInt(valorCaja);
-    return valorInt;
-}
-
-recuperarFloat = function(idComponente){
-    let valorCaja =  recuperarTexto(idComponente);
-    let valorFlotante = parseFloat(valorCaja);
-    return valorFlotante;
-}
