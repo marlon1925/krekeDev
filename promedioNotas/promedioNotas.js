1d65898a9979e2d5da6414b975a1948cc6d6b128
Marlon <marlon.lalangui@epn.edu.ec>
2023-08-27 14:24:36 -0500

    Mostrar reprobado,exitoybuentrabajo, promedionotas

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -7,10 +7,12 @@ calcaularPromedioNotas = function () {
     nota2 = recuperarFlotante("txtNota2");
     nota3 = recuperarFlotante("txtNota3");
     promedio = calcularPromedio(nota1, nota2, nota3);
-    if (promedio >= 7) {
-        cambiarImagen("imgCompror", "../image/exito.gif")
-    }else{
+    if (promedio < 5 && promedio > 0) {
         cambiarImagen("imgCompror", "../image/reprobado.gif")
+    }else if(promedio >= 5 && promedio <= 8){
+        cambiarImagen("imgCompror", "../image/buentrabajo.gif")
+    } else if(promedio > 8 && promedio <= 10){
+        cambiarImagen("imgCompror", "../image/exito.gif")
     }
 
     cambiarTexto("promedio", promedio.toFixed(2));
